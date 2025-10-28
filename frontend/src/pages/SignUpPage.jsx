136e3afafc0a504fe6c24c70a8bcd36b8b3a8be1
PraveshAggarwal <praveshaggarwal10@gmail.com>
2025-10-28 23:19:03 +0530

    fronted

---
+++ frontend/src/pages/SignUpPage.jsx
@@ -0,0 +1,194 @@
+import React, { useState } from "react";
+import { MessagesSquare } from "lucide-react";
+import { Link } from "react-router-dom";
+import useSignUp from "../hooks/useSignUP";
+
+const SignUpPage = () => {
+  const [SignupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  });
+
+  // This is how we did it at first, without using our custom hook
+  // const queryClient = useQueryClient();
+  // const {
+  //   mutate: signupMutation,
+  //   isPending,
+  //   error,
+  // } = useMutation({
+  //   mutationFn: signup,
+  //   onSuccess: () => queryClient.invalidateQueries({ queryKey: ["authUser"] }),
+  // });
+
+  // This is how we did it using our custom hook - optimized version
+  const { isPending, error, signupMutation } = useSignUp();
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    signupMutation(signupData);
+  };
+
+  return (
+    <div
+      className="h-screen flex items-center justify-center p-4 sm:p-6 md:p-8"
+      data-theme="dim"
+    >
+      <div className="border  flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-stone-800 rounded-xl shadow-lg overflow-hidden">
+        {/* SIGNUP FORM -LEFT SIDE */}
+
+        <div className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+          {/* logo */}
+          <div className="mb-4 flex items-center justify-start gap-2">
+            <MessagesSquare className=" size-9 text-white" />
+            <span className="text-3xl font-bold font-mono bg-clip-text text-green-500 bg-linear-to-r from-primary to-secondary tracking-wider">
+              ChatIng
+            </span>
+          </div>
+
+          {/* ERROR MESSAGE IF ANY */}
+          {error && (
+            <div className="alert alert-error mb-4">
+              <span>{error.response.data.message}</span>
+            </div>
+          )}
+
+          <div className="w-full">
+            <form onSubmit={handleSignup}>
+              <div className="space-y-4">
+                <div>
+                  <h2 className="text-xl font-semibold">Create an Account</h2>
+                  <p className="text-sm opacity-70">
+                    Join ChatIng and start your Journey
+                  </p>
+                </div>
+
+                <div className="space-y-3">
+                  {/* Full Name */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text text-white">Full Name</span>
+                    </label>
+                    <input
+                      type="text"
+                      placeholder="Pravesh Aggarwal"
+                      className="input input-bordered w-full"
+                      value={SignupData.fullName}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...SignupData,
+                          fullName: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+
+                  {/* Email */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text text-white">Email</span>
+                    </label>
+                    <input
+                      type="email"
+                      placeholder="pravesh@gmail.com"
+                      className="input input-bordered w-full"
+                      value={SignupData.email}
+                      onChange={(e) =>
+                        setSignupData({ ...SignupData, email: e.target.value })
+                      }
+                      required
+                    />
+                  </div>
+
+                  {/* Password */}
+                  <div className="form-control w-full">
+                    <label className="label">
+                      <span className="label-text text-white">Password</span>
+                    </label>
+                    <input
+                      type="password"
+                      placeholder="********"
+                      className="input input-bordered w-full"
+                      value={SignupData.password}
+                      onChange={(e) =>
+                        setSignupData({
+                          ...SignupData,
+                          password: e.target.value,
+                        })
+                      }
+                      required
+                    />
+                  </div>
+                  <p>Password must be at least 6 characters long</p>
+
+                  <div className="form-control">
+                    <label className="label cursor-pointer justify-start gap-2">
+                      <input
+                        type="checkbox"
+                        className="checkbox checkbox-sm border-white"
+                        required
+                      />
+                      <span className="text-xs leading-tight">
+                        I agree to the{" "}
+                        <span className="text-white hover:underline">
+                          terms of service
+                        </span>{" "}
+                        and{" "}
+                        <span className="text-white hover:underline">
+                          privacy policy
+                        </span>
+                      </span>
+                    </label>
+                  </div>
+                </div>
+
+                <button
+                  className="btn btn-primary w-full bg-green-500"
+                  type="submit"
+                >
+                  {isPending ? "Signing up..." : "Create Account"}
+                </button>
+
+                <div className="text-center mt-4">
+                  <p className="text-sm">
+                    Already have an account?{" "}
+                    <Link to="/login" className="text-white hover:underline">
+                      Sign in
+                    </Link>
+                  </p>
+                </div>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        {/* SIGNUP IMAGE -RIGHT SIDE */}
+        <div className="hidden lg:flex w-full lg:w-1/2 bg-primary/10 items-center justify-center">
+          <div className="max-w-md p-8">
+            {/* Illustration */}
+            <div className="relative aspect-square max-w-sm mx-auto">
+              <img
+                src="/i.png"
+                alt="Language connection illustration"
+                className="w-full h-full"
+              />
+            </div>
+
+            <div className="text-center space-y-3 mt-6">
+              <h2 className="text-xl font-semibold">
+                Connect with language partners worldwide
+              </h2>
+              <p className="opacity-70">
+                Practice conversations, make friends, and improve your language
+                skills together
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPage;
